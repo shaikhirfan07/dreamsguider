91f32bdbc6e0369f54c02b09f06818d7f80257f1
shaikhirfan07 <164344253+shaikhirfan07@users.noreply.github.com>
2024-03-21 17:23:55 +0530

    uploaded change index

---
+++ js.js
@@ -0,0 +1,51 @@
+/*const student={
+     name:"shaikh",
+     age:20,
+     sgps:8.32,
+     ispass:"passed"
+};*/
+/*let a=34
+let b=90
+let c=a+b*/
+/*let a=prompt("choose your enter tables:->");
+let i;
+//let table;
+for(i=1; i<=10; i++)
+{
+    //table=a*i;
+    console.log(a*i);
+    document.write(a*i);
+}*/
+/*const table={
+    let a=prompt("choose your enter tables:->");
+let i;
+//let table;
+for(i=1; i<=10; i++)
+{
+    //table=a*i;
+    console.log(a*i);
+    document.write(a*i);
+}
+};*/
+/*let a=prompt("enter prime number");
+//let prime;
+//let ;
+if(a%2===0)
+{
+console.log("this number  not prime_>",p);
+}else
+{
+    p=2;
+    console.log("this number is  prime_>",p);  
+}*/
+/*let n=prompt("enter prime no");
+let i;
+//let sum;
+let fac=1;
+for(i=1; i<=n; i++)
+{
+    //sum=sum+i;
+    fac=fac*i;
+    console.log("that is facter n number",fac);
+}
+/*
